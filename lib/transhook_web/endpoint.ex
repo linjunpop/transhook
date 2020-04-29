c9141095b9004deac1221b62f53334672f34c68b
Jun Lin <linjunpop@gmail.com>
2020-04-29 16:54:42 +0800

    Ready for Heroku.

--- lib/transhook_web/endpoint.ex
+++ lib/transhook_web/endpoint.ex
@@ -11,7 +11,7 @@ defmodule TranshookWeb.Endpoint do
   ]
 
   socket "/socket", TranshookWeb.UserSocket,
-    websocket: true,
+    websocket: [timeout: 45_000],
     longpoll: false
 
   socket "/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]]
