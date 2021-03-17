db58f47820b8e2ffd7ebaab76322f7febe90c607
Jun Lin <linjunpop@gmail.com>
2021-03-17 19:08:50 +0800

    Rows.

--- mix.exs
+++ mix.exs
@@ -39,13 +39,13 @@ defmodule Transhook.MixProject do
       {:postgrex, ">= 0.0.0"},
       {:phoenix_html, "~> 2.11"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
-      {:phoenix_live_dashboard, "~> 0.2.0"},
+      {:phoenix_live_dashboard, "~> 0.4.0"},
       {:telemetry_metrics, "~> 0.4"},
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:jaxon, "~> 1.0"},
+      {:jaxon, "~> 2.0"},
       {:hackney, "~> 1.15"}
     ]
   end
