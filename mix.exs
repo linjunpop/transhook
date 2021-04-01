46a81605062863e74013474cbaec00aa5ebd0131
Jun Lin <linjunpop@gmail.com>
2021-04-01 18:54:50 +0800

    Setup liveview.

--- mix.exs
+++ mix.exs
@@ -39,6 +39,7 @@ defmodule Transhook.MixProject do
       {:postgrex, ">= 0.0.0"},
       {:phoenix_html, "~> 2.11"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
+      {:phoenix_live_view, "~> 0.15.4"},
       {:phoenix_live_dashboard, "~> 0.4.0"},
       {:telemetry_metrics, "~> 0.4"},
       {:telemetry_poller, "~> 0.4"},
